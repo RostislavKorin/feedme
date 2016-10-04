5641430b122642308c79e6aa411afc664fbf070f
Rostislav Korin <rostislav.korin@gmail.com>
2016-10-04 12:36:41 +0300

    update user

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
 	devise_scope :user do
-  	get 'sign_in', to: 'devise/sessions#new'
-  	get 'sign_out', to: 'devise/sessions#destroy'
-  	get 'sign_up', to: 'devise/registrations#new'
+  	get 'sign_in', to: 'users/sessions#new'
+  	get 'sign_out', to: 'users/sessions#destroy'
+  	get 'sign_up', to: 'users/registrations#new'
+  	get 'edit',		 to: 'devise/registrations#edit'
 	end 
-	devise_for :users
+	devise_for :users, controllers: { sessions: 'users/sessions' }
+	get 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
